b02b7be90ff29c679fa184274361f4e5eb570a3d
Tanapong Udomrattanasirichai <princezidane@PRONTOLAP0026.local>
2014-10-04 08:59:40 +0700

    add Fizzbuzz and Kapook class into feature3 bracnch

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,6 +3,10 @@ class PrintNumber:
         return number * 2
 
 
+class Sanook:
+    pass
+
+
 class FizzBuzz:
     def xx():
         print 'A'
@@ -10,3 +14,9 @@ class FizzBuzz:
         print 'x'
     def zz():
         print 'abc'
+
+
+class Kapook:
+    def printkapook(self):
+        print 'Kapook'
+
